12486a6660a67cbfb1ad25e9690e2e10d6e06dd9
gabrielcoder247 <gabrielcoder247@gmail.com>
2020-09-26 12:38:16 +0100

    writing navigation components

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,8 +1,13 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import Colors from '../constants/Colors';
+import { Platform } from 'react-native';
+
+
 
 const MealsNavigator = createStackNavigator({
   Categories: CategoriesScreen,
@@ -10,6 +15,17 @@ const MealsNavigator = createStackNavigator({
     screen: CategoryMealsScreen
   },
   MealDetail: MealDetailScreen
-});
+}, {
+  // initialRouteName :'categories',
+  defaultNavigatorOptions:{
+    headerStyle:{
+    backgroundColor:Platform.OS === 'android ' ?
+    Colors.primaryColor: ''
+    },
+    headerTintColor:Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+    headerTitle:'A Screen'
+  }
+}
+);
 
 export default createAppContainer(MealsNavigator);
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet,Button, Platform } from 'react-native';
+import Colors from '../constants/Colors';
+import { CATEGORIES } from '../data/dummy-data';
+
 
 const CategoryMealScreen = props => {
+  const catId = props.navigation.getParam('categoryId')
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
+        <Text>{selectedCategory.title}</Text>
+      <Button title='Go to Details' onpress={() => {
+        props.navigation.nagivate({
+          routeName: 'MealDetail'
+        })
+      }}/>
+      <Button title='Go Back' onPress={() => {
+        props.navigation.pop();
+      }}/>
     </View>
   );
+};
+CategoryMealScreen.navigationOptions = navigationData => {
+  const catId = navigationData.navigation.getParam('categoryId');
+  const selectedCategory = CATEGORIES.find(cat => cat.id ===catId);
+  return {
+    headerTitle:selectedCategory.title
+    // headerStyle:{
+    //   backgroundColor:Platform.OS === 'android' ?
+    //   Colors.primaryColor : ''
+    // },
+    // headerTintColor:Platform.OS === 'android' ? 'white' :
+    // Colors.primaryColor
+  };
 };
 
 const styles = StyleSheet.create({
